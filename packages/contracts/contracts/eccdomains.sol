1a3d955016d42db88a7a428aeaef943ea2110fbe
stevegachau.eth <steve@zawadi.co>
2022-12-24 13:59:50 +0300

    Update eccdomains.sol

--- packages/contracts/contracts/eccdomains.sol
+++ packages/contracts/contracts/eccdomains.sol
@@ -1118,20 +1118,7 @@ contract ERC721 is Context, ERC165, IERC721, IERC721Metadata {
         }
     }
 
-    /**
-     * @dev Hook that is called before any token transfer. This includes minting
-     * and burning.
-     *
-     * Calling conditions:
-     *
-     * - When `from` and `to` are both non-zero, ``from``'s `tokenId` will be
-     * transferred to `to`.
-     * - When `from` is zero, `tokenId` will be minted for `to`.
-     * - When `to` is zero, ``from``'s `tokenId` will be burned.
-     * - `from` and `to` are never both zero.
-     *
-     * To learn more about hooks, head to xref:ROOT:extending-contracts.adoc#using-hooks[Using Hooks].
-     */
+   
     function _beforeTokenTransfer(
         address from,
         address to,
