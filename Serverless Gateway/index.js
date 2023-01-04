4f8e8ba0af182199f0269e31495cdb48b2d6e59f
stevegachau.eth <steve@zawadi.co>
2023-01-04 16:54:58 +0300

    Update index.js

--- Serverless Gateway/index.js
+++ Serverless Gateway/index.js
@@ -1,152 +1,159 @@
 const MongoClient = require('mongodb').MongoClient;
 const uri = "........."; //mongodb uri
-let client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true  });
-const clientPromise = client.connect();
 
+let client;
+try {
+  client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true  });
+} catch (error) {
+  console.error(error);
+}
+
+let clientPromise;
+try {
+  clientPromise = client.connect();
+} catch (error) {
+  console.error(error);
+}
 
 exports.helloWorld = async (req, res) => {
+  res.set('Access-Control-Allow-Origin', '*');
 
-res.set('Access-Control-Allow-Origin', '*');
-
-if (req.method === 'OPTIONS') {
+  if (req.method === 'OPTIONS') {
     // Send response to OPTIONS requests
     res.set('Access-Control-Allow-Methods', 'POST');
     res.set('Access-Control-Allow-Headers', 'Content-Type');
     res.set('Access-Control-Max-Age', '3600');
     res.status(204).send('');
-} 
+    return;
+  } 
+
+  const Web3 = require('web3');
+  const ethers = require('ethers');
+  const InputDataDecoder = require('ethereum-input-data-decoder');
+
+  let domainaddress;
+  let domaincontract;
+  let web3;
+  let mongoaddr;
+  try {
+    web3 = new Web3(''); //Optimism RPC url
+  } catch (error) {
+    console.error(error);
+  }
+
+  // ABI to decode the ETH Calldata
+  const decoderabi = [];
+
+  let decoder;
+  try {
+    decoder = new InputDataDecoder(decoderabi);
+  } catch (error) {
+    console.error(error);
+  }
+
+  // ABI of the deployed offchainresolver contract on L1 (contract code: https://etherscan.io/address/0x9fb848300e736075ed568147259bf8a8eefe4fef#code)
+  const Resolver_abi = [];
+
+  let Resolver;
+  try {
+    Resolver = new ethers.utils.Interface(Resolver_abi);
+  } catch (error) {
+    console.error(error);
+  }
+
+  // ABI of the deployed Subdomain NFT on L2 (contract code: https://optimistic.etherscan.io/address/0x9fb848300e736075ed568147259bf8a8eefe4fef#code)
+  const domainabi = [];
+
+  const sender = req.query.sender;
+  const resolverdata = req.query.data;
+
+  let dataresult;
+  try {
+    dataresult = await decoder.decodeData(resolverdata);
+  } catch (error) {
+    console.error(error);
+  }
+
+  const hexname = dataresult.inputs[0];
+  const data = dataresult.inputs[1];
+
+  let utf8;
+  try {
+    utf8 = await web3.utils.hexToUtf8(hexname);
+  } catch (error) {
+    console.error(error);
+      
+    const sliced = utf8.slice(1);
+  const domain = sliced.replace(/[\x00\x01\x02\x03\x04\x05]/g," ");
+  const fulldomain = domain.trim().split(/\s+/);
+
+  const sub = fulldomain[0];
+  const tld = fulldomain[1];
+  const ext = fulldomain[2];
+
+  const name = sub+"."+tld+"."+ext;
+
+  const privateKey = ""; // privKey of signing address used during contract deploy of resolver contract
+  let signer;
+  try {
+    signer = new ethers.utils.SigningKey(privateKey);
+  } catch (error) {
+    console.error(error);
+  }
+
+  if (tld == "ecc") { 
+    try {
+      domainaddress = ""; //subdomain NFT on Optimism. 
+      web3 = new Web3(''); //optimism RPC url
+      const account = web3.eth.accounts.wallet.add(privateKey);
+      web3.eth.defaultAccount = account.address;
+      domaincontract = new web3.eth.Contract(domainabi, domainaddress, {
+        from: '' //from address
+      });
+    } catch (error) {
+      console.error(error);
+    }
+  }
+
+  async function handler(args) {
+    if (args.length == 1) { 
+      let addr;
+      try {
+        addr = await domaincontract.methods.resolve(sub).call();
+      } catch (error) {
+        console.error(error);
+      }
+      return { result: [addr] };
+    } else if (args[1].toString() == 60 ) { 
+      let addr;
+      try {
+        addr = await domaincontract.methods.resolve(sub).call();
+      } catch (error) {
+        console.error(error);
+      }
+      return { result: [addr] };
+    }
   
-else {
+    // Implement your own custom logic to return offchain ENS records depending on where offchain data is stored.
+    else if (args[1].toString() == 0 ) {
+      const addr = "0x0000000000000000000000000000000000000000";
+      return { result: [addr] };
+    } else if (args[1].toString() == 2 ) {
+      const addr = "0x0000000000000000000000000000000000000000";
+      return { result: [addr] };
+    }
+    // Add more cases here as needed
+    else {
+      return { result: [] };
+    }
+  }
 
-
-var sender = req.query.sender;
-var resolverdata = req.query.data;
-const Web3 = require('web3');
-const ethers = require('ethers');
-const InputDataDecoder = require('ethereum-input-data-decoder');
-var domainaddress;
-var domaincontract;
-var web3;
-var mongoaddr;
-web3 = new Web3(''); //Optimism RPC url
-
-
-//ABI to decode the ETH Calldata
-const decoderabi = [];
-    
-const decoder = new InputDataDecoder(decoderabi);
-
-
-//ABI of the deployed offchainresolver contract on L1 (contract code: https://etherscan.io/address/0x9fb848300e736075ed568147259bf8a8eefe4fef#code)
-const Resolver_abi = [];
-    
-const Resolver = new ethers.utils.Interface(Resolver_abi);
-
-
-//ABI of the deployed Subdomain NFT on L2 (contract code: https://optimistic.etherscan.io/address/0x9fb848300e736075ed568147259bf8a8eefe4fef#code)
-const domainabi = [];
-
-
-
-var dataresult = await decoder.decodeData(resolverdata);
-var hexname = dataresult.inputs[0];
-var data = dataresult.inputs[1];
-var utf8 = await web3.utils.hexToUtf8(hexname);
-var sliced = utf8.slice(1);
-var domain = sliced.replace(/[\x00\x01\x02\x03\x04\x05]/g," ");
-var fulldomain = domain.trim().split(/\s+/);
-
-var sub = fulldomain[0];
-var tld = fulldomain[1];
-var ext = fulldomain[2];
-
-var name = sub+"."+tld+"."+ext;
-
-
-const privateKey = ""; //privKey of signing address used during contract deploy of resolver contract on l2 
-const signer = new ethers.utils.SigningKey(privateKey);
-
-if (tld == "ecc"){ 
-  
-domainaddress = ""; //subdomain NFT on Optimism. 
-web3 = new Web3(''); //optimism RPC url
-const account = web3.eth.accounts.wallet.add(privateKey);
-web3.eth.defaultAccount = account.address;
-domaincontract = new web3.eth.Contract(domainabi, domainaddress, {
-    from: '' //from address
-    })
-}
-
-
-
-async function handler(args) {
-
- if (args.length == 1){ 
-   var addr = await domaincontract.methods.resolve(sub).call();
-   return { result: [addr] };
- }
-
- else if (args[1].toString() == 60 ) { 
-   var addr = await domaincontract.methods.resolve(sub).call();
-   return { result: [addr] };
- }
-  
-  
- //implement your own custom logic to return offchain ENS records depending on where offchain data is stored.
-  
- else if (args[1].toString() == 0 ) { var addr = "0x0000000000000000000000000000000000000000"; return { result: [addr] }; }
- else if (args[1].toString() == 2 ) { var addr = "0x0000000000000000000000000000000000000000"; return { result: [addr] }; }
- else if (args[1].toString() == 3 ) { var addr = "0x0000000000000000000000000000000000000000"; return { result: [addr] }; }
- else if (args[1] == "url") { var addr = ""; return { result: [addr] }; }
- else if (args[1] == "email") { var addr = ""; return { result: [addr] }; }
- else if (args[1] == "avatar") { var addr = ""; return { result: [addr] }; }
- else if (args[1] == "description") { var addr = ""; return { result: [addr] }; }
- else if (args[1] == "notice") { var addr = ""; return { result: [addr] }; }
- else if (args[1] == "keywords") { var addr = ""; return { result: [addr] }; }
- else if (args[1] == "com.discord") { var addr = ""; return { result: [addr] }; }
- else if (args[1] == "com.twitter") { var addr = ""; return { result: [addr] }; }
- else if (args[1] == "com.github") { var addr = ""; return { result: [addr] }; }
- else if (args[1] == "com.reddit") { var addr = ""; return { result: [addr] }; }
- else if (args[1] == "org.telegram") { var addr = ""; return { result: [addr] }; }
- else if (args[1] == "eth.ens.delegate") { var addr = ""; return { result: [addr] }; }
- else {
-   console.log(args);
-   res.status(200).send();}
+  try {
+    const result = await handler(data);
+    res.send(result);
+  } catch (error) {
+    console.error(error);
+  }
 };
 
-
-async function query(name, data){
-
-  const { signature, args } = Resolver.parseTransaction({ data });
-  const { result } = await handler(args);
-
-  return {
-    result: Resolver.encodeFunctionResult(signature, result),
-    validUntil: Math.floor(Date.now() / 1000 ),
-  };
-
-}
-
-
-const { result, validUntil } = await query(name, data);
-
-let messageHash = ethers.utils.solidityKeccak256(
-          ['bytes', 'address', 'uint64', 'bytes32', 'bytes32'],
-          [
-            '0x1900',
-            sender,
-            validUntil,
-            ethers.utils.keccak256(resolverdata || '0x'),
-            ethers.utils.keccak256(result),
-          ]
-        );
-
-
-const sig = signer.signDigest(messageHash);
-const sigData = ethers.utils.hexConcat([sig.r, sig._vs]);
-const returndata = web3.eth.abi.encodeParameters(['bytes','uint64','bytes'], [result, validUntil, sigData]);
-
-
-res.status(200).send({"data":returndata});
-}
-}
+ 
